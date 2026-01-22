c52c01461d6637d699411a96a8a5a23bd9abfa10
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-01-22 17:10:22 -0300

    Add files via upload

---
+++ principal.hpp
@@ -0,0 +1,21 @@
+#ifndef _TPRINCIPAL_T_
+#define _TPRINCIPAL_T_
+
+#include <iostream>
+#include <stdio.h>
+#include "biblioteca/funciones/strings.hpp"
+#include "biblioteca/funciones/tokens.hpp"
+#include "biblioteca/funciones/files.hpp"
+#include "biblioteca/tads/Array.hpp"
+#include "biblioteca/tads/Map.hpp"
+#include "biblioteca/tads/List.hpp"
+#include "biblioteca/tads/Stack.hpp"
+#include "biblioteca/tads/Queue.hpp"
+#include "biblioteca/tads/Coll.hpp"
+#include "principal.cpp"
+
+
+
+
+
+#endif
