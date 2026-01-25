ada84a99653f0809108c07d13e1c2d2103492f6d
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-01-25 16:37:42 -0300

    Actualizar infoByte.hpp

--- infoByte.hpp
+++ infoByte.hpp
@@ -1,7 +1,22 @@
 #ifndef INFOBYTE_HPP
 #define INFOBYTE_HPP
 
-#include <string>
+#include <string.h>
+#include "biblioteca/funciones/strings.hpp"
+#include "biblioteca/funciones/arrays.hpp"
+#include "biblioteca/funciones/lists.hpp"
+#include "biblioteca/funciones/tokens.hpp"
+#include "biblioteca/funciones/files.hpp"
+#include "biblioteca/tads/Array.hpp"
+#include "biblioteca/tads/Map.hpp"
+#include "biblioteca/tads/List.hpp"
+#include "biblioteca/tads/Stack.hpp"
+#include "biblioteca/tads/Queue.hpp"
+#include "biblioteca/tads/Coll.hpp"
+#include "biblioteca/tads/huffman/HuffmanSetup.hpp"
+#include "biblioteca/tads/huffman/BitReader.hpp"
+#include "biblioteca/tads/huffman/BitWriter.hpp"
+
 using namespace std;
 
 struct InfoByte
