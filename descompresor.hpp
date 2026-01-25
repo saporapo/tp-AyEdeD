4e5c749a07ceba9caecaa63431dcfbdd3900c614
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-01-25 16:48:01 -0300

    Actualizar descompresor.hpp

--- descompresor.hpp
+++ descompresor.hpp
@@ -1,24 +1,6 @@
 #ifndef _TDESCOMPRESOR_T_
 #define _TDESCOMPRESOR_T_
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "biblioteca/funciones/strings.hpp"
-#include "biblioteca/funciones/arrays.hpp"
-#include "biblioteca/funciones/lists.hpp"
-#include "biblioteca/funciones/tokens.hpp"
-#include "biblioteca/funciones/files.hpp"
-#include "biblioteca/tads/Array.hpp"
-#include "biblioteca/tads/Map.hpp"
-#include "biblioteca/tads/List.hpp"
-#include "biblioteca/tads/Stack.hpp"
-#include "biblioteca/tads/Queue.hpp"
-#include "biblioteca/tads/Coll.hpp"
-#include "biblioteca/tads/huffman/HuffmanSetup.hpp"
-#include "biblioteca/tads/huffman/BitReader.hpp"
-#include "biblioteca/tads/huffman/BitWriter.hpp"
 #include "infoByte.hpp"
 
 using namespace std;
